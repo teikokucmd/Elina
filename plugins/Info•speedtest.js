46601e56db77e9a41564ce43a667ff7a6906de31
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-10-20 12:09:23 -0500

    Update Info•speedtest.js

--- plugins/Info•speedtest.js
+++ plugins/Info•speedtest.js
@@ -1,36 +1,32 @@
-import cp from 'child_process';
-import { promisify } from 'util';
-const exec = promisify(cp.exec).bind(cp);
+import cp from 'child_process'
+import {promisify} from 'util'
 
-const handler = async (m) => {
-    let o;
-    try {
-      conn.reply(m.chat, '🚀 Speed Test....', m, {
-     contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-     title: packname,
-     body: dev,
-     previewType: 0, thumbnail: icons, 
-     sourceUrl: channel }}})
-      o = await exec('python3 ./lib/ookla-speedtest.py --secure --share');
-        const {stdout, stderr} = o;
-        if (stdout.trim()) {
-            const match = stdout.match(/http[^"]+\.png/);
-            const urlImagen = match ? match[0] : null;
-            await conn.sendMessage(m.chat, {image: {url: urlImagen}, caption: stdout.trim()}, {quoted: fkontak});
-        }
-        if (stderr.trim()) { 
-            const match2 = stderr.match(/http[^"]+\.png/);
-            const urlImagen2 = match2 ? match2[0] : null;    
-            await conn.sendMessage(m.chat, {image: {url: urlImagen2}, caption: stderr.trim()}, {quoted: fkontak});
-        }
-    } catch (e) {
-        o = e.message;
-        return m.reply(o)
-    }
-};
-handler.help = ['speedtest'];
-handler.tags = ['info'];
-handler.command = ['speedtest', 'info', 'speed'];
-handler.register = true;
+const exec = promisify(cp.exec).bind(cp)
 
-export default handler;
+var handler = async (m) => {
+
+conn.reply(m.chat, '☁️ Espere un momento soy lenta..', m)
+
+let o
+  
+try {
+
+o = await exec('python3 speed.py --secure --share');
+} catch (e) {
+o = e
+} finally {
+let {stdout, stderr} = o
+
+
+if (stdout.trim()) conn.reply(m.chat, stdout, m)
+if (stderr.trim()) conn.reply(m.chat, stderr, m)
+}
+
+}
+handler.help = ['speedtest']
+handler.tags = ['bot']
+handler.command = /^(speedtest?|test?speed)$/i
+
+handler.register = true
+
+export default handler
