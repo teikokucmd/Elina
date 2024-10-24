7774b3adaf6b202569666e736a62c7a85a2840e1
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-10-24 11:38:57 -0500

    Update Config•nable.js

--- plugins/Config•nable.js
+++ plugins/Config•nable.js
@@ -235,46 +235,26 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
       }
       chat.antiLink = isEnable
       break
-
-      case 'nsfw':
-      case 'modohorny':
-       if (m.isGroup) {
-         if (!(isAdmin || isOwner)) {
-           global.dfail('admin', m, conn)
-            throw false
-           }}
-    chat.modohorny = isEnable          
-    break
     default:
       if (!/[01]/.test(command)) return conn.reply(m.chat, `
-*👑 Funciones solo para owner*
+*ゲ◜៹ Bot Creadores ៹◞ゲ*
 
 ${usedPrefix + command} antispam
 ${usedPrefix + command} antiprivado
-${usedPrefix + command} subbots
 ${usedPrefix + command} status
 ${usedPrefix + command} restrict
-${usedPrefix + command} autoread
 
-*🚩 Funciones de grupos*
+*ゲ◜៹ Bot Administrador ៹◞ゲ*
 
 ${usedPrefix + command} welcome 
 ${usedPrefix + command} antibot
 ${usedPrefix + command} reaccion
-${usedPrefix + command} simi
-${usedPrefix + command} audios
-${usedPrefix + command} autoread
-${usedPrefix + command} antiver
-${usedPrefix + command} detect 
-${usedPrefix + command} delete
-${usedPrefix + command} nsfw 
-${usedPrefix + command} antitraba
-${usedPrefix + command} modoadmin 
-${usedPrefix + command} antiarabes 
+${usedPrefix + command} simi  
+${usedPrefix + command} modoadmin  
 ${usedPrefix + command} antilink`, m, rcanal)
       throw false
   }
-  conn.reply(m.chat, `🚩 La función *${type}* se *${isEnable ? 'activó' : 'desactivó'}* ${isAll ? 'para este Bot' : isUser ? '' : 'para este chat'}`, m, rcanal)
+  conn.reply(m.chat, `🍿 La función *${type}* se *${isEnable ? 'activó' : 'desactivó'}* ${isAll ? 'para este Bot' : isUser ? '' : 'para este chat'}`, m, rcanal)
 }
 
 handler.help = ['enable', 'disable']
