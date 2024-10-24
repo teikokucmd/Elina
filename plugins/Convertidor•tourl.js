4552939096608520480f403045950f023a114cc1
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-10-24 11:44:34 -0500

    Update Convertidor•tourl.js

--- plugins/Convertidor•tourl.js
+++ plugins/Convertidor•tourl.js
@@ -5,7 +5,7 @@ import fetch from 'node-fetch'
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) return conn.reply(m.chat, '🚩 Responde a una *Imagen* o *Vídeo.*', m, rcanal)
+  if (!mime) return conn.reply(m.chat, '🍿 Responde a una Imagen', m, rcanal)
   await m.react(rwait)
   try {
   /*conn.reply(m.chat, '🚩 Convirtiendo la imagen en url...', m, {
