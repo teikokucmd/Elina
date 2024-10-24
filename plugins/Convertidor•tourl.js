53d468dbc246209f233ac09738cbcc24e914e368
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-10-24 11:43:06 -0500

    Update Convertidor•tourl.js

--- plugins/Convertidor•tourl.js
+++ plugins/Convertidor•tourl.js
@@ -8,12 +8,12 @@ let handler = async (m) => {
   if (!mime) return conn.reply(m.chat, '🚩 Responde a una *Imagen* o *Vídeo.*', m, rcanal)
   await m.react(rwait)
   try {
-  conn.reply(m.chat, '🚩 Convirtiendo la imagen en url...', m, {
+  /*conn.reply(m.chat, '🚩 Convirtiendo la imagen en url...', m, {
   contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
   title: packname,
   body: dev,
   previewType: 0, thumbnail: icons,
-  sourceUrl: channel }}})
+  sourceUrl: channel }}})*/
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
