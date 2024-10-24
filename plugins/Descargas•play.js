ac838dab11f3e1b7ac4dd4c045e445f95a7cfd0f
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-10-24 12:02:52 -0500

    Update Descargas•play.js

--- plugins/Descargas•play.js
+++ plugins/Descargas•play.js
@@ -3,7 +3,7 @@ import yts from "yt-search";
 
 const handler = async (m, {conn, command, args, text, usedPrefix}) => {
 
-if (!text) return conn.reply(m.chat, `🚩 *Ingrese el nombre de un video de YouTube*\n\nEjemplo, !${command} Distancia - Kimberly Contreraxx`,  m, rcanal, )
+if (!text) return conn.reply(m.chat, `🍿 Ingrese el nombre de una *Musica* o *Video*`,  m, rcanal, )
 
 /*conn.reply(m.chat, global.wait, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
