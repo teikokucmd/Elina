bd56fced7a628d10d52f370c9ca2720f491b3ad9
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-10-27 16:17:12 -0500

    Update Descargas•ytv.js

--- plugins/Descargas•ytv.js
+++ plugins/Descargas•ytv.js
@@ -1,123 +1,143 @@
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
-import fetch from 'node-fetch'
-import yts from 'yt-search'
-import ytdl from 'ytdl-core'
-import axios from 'axios'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-if (!args[0]) return conn.reply(m.chat, `🚩 Te Faltó Un Link De Un Video De Youtube`, /*fkontak,*/ m, { contextInfo: { 'forwardingScore': 0, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: packname, body: `👋 Hola ` + nombre, mediaType: 3, sourceUrl: redes, thumbnail: icons}}}, { quoted: fkontak })
+import axios from 'axios';
+import yts from 'yt-search';
 
-let youtubeLink = '';
-if (args[0].includes('you')) {
-youtubeLink = args[0];
-} else {
-const index = parseInt(args[0]) - 1;
-if (index >= 0) {
-if (Array.isArray(global.videoList) && global.videoList.length > 0) {
-const matchingItem = global.videoList.find(item => item.from === m.sender);
-if (matchingItem) {
-if (index < matchingItem.urls.length) {
-youtubeLink = matchingItem.urls[index];
-} else {
-throw `🌀 *no se encontró*`;
-}} else {
-throw `𝙋𝘼𝙍𝘼 𝙋𝙊𝘿𝙀𝙍 𝙐𝙎𝘼𝙍 𝙀𝙎𝙏𝙀 𝘾𝙊𝙈𝘼𝙉𝘿𝙊 𝘿𝙀 𝙀𝙎𝙏𝘼 𝙁𝙊𝙍𝙈𝘼 (${usedPrefix + command} <numero>), 𝙋𝙊𝙍 𝙁𝘼𝙑𝙊𝙍 𝙍𝙀𝘼𝙇𝙄𝙕𝘼𝙍 𝙇𝘼 𝘽𝙐́𝙎𝙌𝙐𝙀𝘿𝘼 𝘿𝙀 𝙑𝙄́𝘿𝙀𝙊𝙎 𝘾𝙊𝙉 𝙀𝙇 𝘾𝙊𝙈𝘼𝙉𝘿𝙊  ${usedPrefix}playlist <texto>*`;
-}} else {
-throw `𝙋𝘼𝙍𝘼 𝙋𝙊𝘿𝙀𝙍 𝙐𝙎𝘼𝙍 𝙀𝙎𝙏𝙀 𝘾𝙊𝙈𝘼𝙉𝘿𝙊 𝘿𝙀 𝙀𝙎𝙏𝘼 𝙁𝙊𝙍𝙈𝘼 (${usedPrefix + command} <numero>), 𝙋𝙊𝙍 𝙁𝘼𝙑𝙊𝙍 𝙍𝙀𝘼𝙇𝙄𝙕𝘼𝙍 𝙇𝘼 𝘽𝙐́𝙎𝙌𝙐𝙀𝘿𝘼 𝘿𝙀 𝙑𝙄́𝘿𝙀𝙊𝙎 𝘾𝙊𝙉 𝙀𝙇 𝘾𝙊𝙈𝘼𝙉𝘿𝙊  ${usedPrefix}playlist <texto>*`;
-}}}  
+const getVideoId = (url) => {
+  const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/[^\/\n\s]+\/|(?:v|e(?:mbed)?)\/|[^v\r\s]+\/|user\/[^\/\n\s]+|embed\/|videoseries\?list=)|(?:youtu\.)?be(?:\.com)?\/(?:watch\?v=|v\/|u\/\w\/|embed\/|watch\?v%3D|watch\?v-|watch\/|v=)?)((\w|-){11}).*/;
+  const match = url.match(regex);
+  if (match) {
+    return match[1];
+  }
+  throw new Error("Link inválido de YouTube");
+};
 
-await conn.reply(m.chat, `🌺 E S P E R E\n- 🍃 Se está descargando su video, espere un momento..`, m, fake)
-try {
-let qu = args[1] || '360'
-let q = qu + 'p'
-let v = youtubeLink
-const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v))
-const dl_url = await yt.video[q].download()
-const ttl = await yt.title
-const size = await yt.video[q].fileSizeH
-await await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `╭━❰  ${packname}  ❱━⬣\n┃ 💜 𝙏𝙄𝙏𝙐𝙇𝙊\n┃ ${ttl}\n╰━━━━━❰ *${vs}* ❱━━━━⬣}`, thumbnail: await fetch(yt.thumbnail) }, { quoted: m })
-} catch (E1) {
-//console.log('Error 1 ' + E1)  
-try {  
-let mediaa = await ytMp4(youtubeLink)
-await conn.sendMessage(m.chat, { video: { url: mediaa.result }, fileName: `error.mp4`, caption: `${packname}`, thumbnail: mediaa.thumb, mimetype: 'video/mp4' }, { quoted: m })     
-} catch (E2) {  
-//console.log('Error 2 ' + E2)   
-try {
-let lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytvideo2?apikey=${lolkeysapi}&url=${youtubeLink}`)    
-let lolh = await lolhuman.json()
-let n = lolh.result.title || 'error'
-let n2 = lolh.result.link
-let n3 = lolh.result.size
-let n4 = lolh.result.thumbnail
-await conn.sendMessage(m.chat, { video: { url: n2 }, fileName: `${n}.mp4`, mimetype: 'video/mp4', caption: `╭━❰  ${packname}  ❱━⬣\n┃ 💜 𝙏𝙄𝙏𝙐𝙇𝙊\n┃ ${n}\n╰━━━━━❰ *${vs}* ❱━━━━⬣`, thumbnail: await fetch(n4) }, { quoted: m })
-} catch (E3) {
-//console.log('Error 3 ' + E3)   
-await conn.reply(m.chat, `𝙀𝙎 𝙋𝙊𝙎𝙄𝘽𝙇𝙀 𝙌𝙐𝙀 𝙀𝙇 𝘼𝙍𝘾𝙃𝙄𝙑𝙊 𝙎𝙀𝘼 𝙈𝙐𝙔 𝙋𝙀𝙎𝘼𝘿𝙊. 𝙄𝙉𝙏𝙀𝙉𝙏𝙀 𝘾𝙊𝙉 𝙊𝙏𝙍𝘼 𝙊𝙋𝘾𝙄𝙊𝙉 𝘿𝙀 𝘿𝙀𝙎𝘾𝘼𝙍𝙂𝘼\n\n𝙄𝙏 𝙄𝙎 𝙋𝙊𝙎𝙎𝙄𝘽𝙇𝙀 𝙏𝙃𝘼𝙏 𝙏𝙃𝙀 𝙁𝙄𝙇𝙀 𝙄𝙎 𝙑𝙀𝙍𝙔 𝙃𝙀𝘼𝙑𝙔. 𝙏𝙍𝙔 𝘼𝙉𝙊𝙏𝙃𝙀𝙍`, m, { contextInfo: { 'forwardingScore': 0, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: packname, body: `👋 Hola ` + nombre, mediaType: 3, sourceUrl: redes, thumbnail: icons}}}, { quoted: fkontak })}
-}}}
-handler.tags = ['descargas']
-handler.help = ['ytmp4 *<url youtube>*']
-handler.command = ['video', 'fgmp4', 'dlmp4', 'getvid', 'yt', 'ytmp4', 'mp4']
-export default handler
+const Ytdl = {
+  search: async (query) => {
+    try {
+      const { videos } = await yts(query);
+      return {
+        status: true,
+        creator: "ola",
+        data: videos.map(video => ({
+          title: video.title,
+          url: `https://youtu.be/${video.videoId}`,
+          img: video.image,
+          author: {
+            name: video.author.name,
+            url: video.author.url,
+          },
+        })),
+      };
+    } catch (error) {
+      return {
+        status: false,
+        msg: "No se pudo obtener los datos!",
+        err: error.message,
+      };
+    }
+  },
 
-function bytesToSize(bytes) {
-return new Promise((resolve, reject) => {
-const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-if (bytes === 0) return 'n/a';
-const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
-if (i === 0) resolve(`${bytes} ${sizes[i]}`);
-resolve(`${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`)})};
+  mp3: async (url, { mp3 = '192' } = {}) => {
+    try {
+      const videoId = getVideoId(url);
+      const videoData = (await yts(videoId)).videos[0];
+      const data = new URLSearchParams({ videoid: videoId, downtype: 'mp3', vquality: mp3 });
 
-async function ytMp3(url) {
-return new Promise((resolve, reject) => {
-ytdl.getInfo(url).then(async(getUrl) => {
-let result = [];
-for(let i = 0; i < getUrl.formats.length; i++) {
-let item = getUrl.formats[i];
-if (item.mimeType == 'audio/webm; codecs=\"opus\"') {
-let { contentLength } = item;
-let bytes = await bytesToSize(contentLength);
-result[i] = { audio: item.url, size: bytes }}};
-let resultFix = result.filter(x => x.audio != undefined && x.size != undefined) 
-let tiny = await axios.get(`https://tinyurl.com/api-create.php?url=${resultFix[0].audio}`);
-let tinyUrl = tiny.data;
-let title = getUrl.videoDetails.title;
-let thumb = getUrl.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url;
-resolve({ title, result: tinyUrl, result2: resultFix, thumb })}).catch(reject)})}
+      const response = await axios.post('https://api-cdn.saveservall.xyz/ajax-v2.php', data, {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' },
+      });
 
-async function ytMp4(url) {
-return new Promise(async(resolve, reject) => {
-ytdl.getInfo(url).then(async(getUrl) => {
-let result = [];
-for(let i = 0; i < getUrl.formats.length; i++) {
-let item = getUrl.formats[i];
-if (item.container == 'mp4' && item.hasVideo == true && item.hasAudio == true) {
-let { qualityLabel, contentLength } = item;
-let bytes = await bytesToSize(contentLength);
-result[i] = { video: item.url, quality: qualityLabel, size: bytes }}};
-let resultFix = result.filter(x => x.video != undefined && x.size != undefined && x.quality != undefined) 
-let tiny = await axios.get(`https://tinyurl.com/api-create.php?url=${resultFix[0].video}`);
-let tinyUrl = tiny.data;
-let title = getUrl.videoDetails.title;
-let thumb = getUrl.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url;
-resolve({ title, result: tinyUrl, rersult2: resultFix[0].video, thumb })}).catch(reject)})};
+      const mp3Link = response.data.url;
+      return {
+        status: true,
+        creator: "ola",
+        msg: "Se descargó el contenido con exito!",
+        title: videoData.title,
+        thumbnail: videoData.image,
+        url: `https://youtu.be/${videoId}`,
+        media: mp3Link,
+      };
 
-async function ytPlay(query) {
-return new Promise((resolve, reject) => {
-yts(query).then(async(getData) => {
-let result = getData.videos.slice( 0, 5 );
-let url = [];
-for (let i = 0; i < result.length; i++) { url.push(result[i].url) }
-let random = url[0];
-let getAudio = await ytMp3(random);
-resolve(getAudio)}).catch(reject)})};
+    } catch (error) {
+      return {
+        status: false,
+        msg: "No se pudo obtener los datos!",
+        err: error.message,
+      };
+    }
+  },
 
-async function ytPlayVid(query) {
-return new Promise((resolve, reject) => {
-yts(query).then(async(getData) => {
-let result = getData.videos.slice( 0, 5 );
-let url = [];
-for (let i = 0; i < result.length; i++) { url.push(result[i].url) }
-let random = url[0];
-let getVideo = await ytMp4(random);
-resolve(getVideo)}).catch(reject)})};
+  mp4: async (url, { mp4 = '480' } = {}) => {
+    try {
+      const videoId = getVideoId(url);
+      const videoData = (await yts(videoId)).videos[0];
+      const data = new URLSearchParams({ videoid: videoId, downtype: 'mp4', vquality: mp4 });
+
+      const response = await axios.post('https://api-cdn.saveservall.xyz/ajax-v2.php', data, {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' },
+      });
+
+      const mp4Link = response.data.url;
+      return {
+        status: true,
+        creator: "ola",
+        msg: "Se descargó el contenido con éxito!",
+        title: videoData.title,
+        thumbnail: videoData.image,
+        url: `https://youtu.be/${videoId}`,
+        media: mp4Link,
+      };
+
+    } catch (error) {
+      return {
+        status: false,
+        msg: "No se pudo obtener los datos!",
+        err: error.message,
+      };
+    }
+  },
+};
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw m.reply(`Ejemplo de uso: ${usedPrefix + command} <link de YouTube>`);
+
+  let result;
+  if (command === 'ytmp3') {
+    result = await Ytdl.mp3(text);
+  } else if (command === 'ytmp4') {
+    result = await Ytdl.mp4(text);
+  } else {
+    result = await Ytdl.search(text);
+  }
+
+  if (!result.status) throw result.msg;
+
+  if (command === 'ytmp3') {
+    const doc = {
+      audio: { url: result.media },
+      mimetype: 'audio/mp4',
+      fileName: `${result.title}.mp3`,
+      contextInfo: {
+        externalAdReply: {
+          showAdAttribution: true,
+          mediaType: 2,
+          mediaUrl: result.url,
+          title: result.title,
+          sourceUrl: result.url,
+          thumbnail: await (await conn.getFile(result.thumbnail)).data
+        }
+      }
+    };
+    await conn.sendMessage(m.chat, doc, { quoted: m });
+  } else if (command === 'ytmp4') {
+    await conn.sendMessage(m.chat, { video: { url: result.media }, caption: result.title }, { quoted: m });
+  } else {
+    let searchResults = result.data.map((v, i) => `${i + 1}. *${v.title}*\n   Link: ${v.url}`).join('\n\n');
+    await conn.sendMessage(m.chat, { text: searchResults }, { quoted: m });
+  }
+};
+
+handler.help = ['ytmp3', 'ytmp4', 'ytsearch'];
+handler.tags = ['downloader'];
+handler.command = /^yt(mp3|mp4|search)$/i;
+
+export default handler;
