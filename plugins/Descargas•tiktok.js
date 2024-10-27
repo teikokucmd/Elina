f97a58f3872be8564c09f92839b147151976f958
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-10-27 01:12:54 -0500

    Update Descargas•tiktok.js

--- plugins/Descargas•tiktok.js
+++ plugins/Descargas•tiktok.js
@@ -1,49 +1,22 @@
-import fetch from 'node-fetch'
+import axios from 'axios'
 
-let handler = async (m, { conn, usedPrefix, command, args }) => {
-if (!args[0]) return m.reply(`Ingresa un enlace de tiktok`)
-
+let handler = async (m, { conn, args }) => {
+if (!args[0]) return m.reply('https://vm.tiktok.com/ZMhNNeDHU/')
 try {
-let api = await fetch(`https://deliriussapi-oficial.vercel.app/download/tiktok?url=${args[0]}`)
-let json = await api.json()
-let { data, process } = json
+let api = await axios.get(`https://api.ryzendesu.vip/api/downloader/ttdl?url=${encodeURIComponent(args[0])}`)
+let json = api.data
+let { data, processed_time:proceso } = json
+let { play:video, duration:duracion, title: titulo, music:audio } = data
 
-let JT = `*Proceso:* ${process}
-*Título:* ${data.title || "No disponible"}
-*Duración:* ${data.duration} segundos
-*Reproducciones:* ${data.repro}
-*Likes:* ${data.like}
-*Compartidos:* ${data.share}
-*Comentarios:* ${data.comment}
-*Descargas:* ${data.download}
-*Fecha de publicación:* ${data.published}
-
-*[ INFO DEL AUTOR ]*\n
-*Username:* ${data.author.username}
-*Apodo:* ${data.author.nickname}
-
-*[ MÚSICA ]*\n
-*Título de la música:* ${data.music.title}
-*Autor de la música:* ${data.music.author}
-*Duración de la música:* ${data.music.duration} segundos`
-
-let tipoArchivo = data.meta.media[0].type
-
-if (tipoArchivo === "video") {
-await conn.sendFile(m.chat, data.meta.media[0].hd, 'video.mp4', JT, m)
-} else if (tipoArchivo === "image") {
-let images = data.meta.media[0].images
-for (let i = 0; i < images.length; i++) {
-await conn.sendFile(m.chat, images[i], `image${i + 1}.jpeg`, `*Imagen :* ${i + 1}`, m)
-}}
-
-//Calidad Normal: ${data.meta.media[0].org} Tamaño: ${data.meta.media[0].size_org}
-//Calidad Media: ${data.meta.media[0].wm} Tamaño: ${data.meta.media[0].size_wm}
-//HD: ${data.meta.media[0].hd} Tamaño: ${data.meta.media[0].size_hd}
+let JT = `*Titulo:* ${titulo}
+*Proceso:* ${proceso} ms
+*Duracion:* ${duracion}`
+await conn.sendFile(m.chat, video, 'HasumiBotFreeCodes.mp4', JT, m)
+await conn.sendFile(m.chat, audio, 'HasumiBotFreeCodes.mp3', null, m)
 } catch (error) {
-console.error(error)
+console.error(error)    
 }}
 
-handler.command = /^(tiktok)$/i;
+handler.command = /^(tiktok)$/i
 
 export default handler
