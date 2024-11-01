de02def201361284fad2f9eb9bcbe4939ac8528d
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-11-01 16:39:05 -0500

    Update Descargas•instagram.js

--- plugins/Descargas•instagram.js
+++ plugins/Descargas•instagram.js
@@ -1,29 +1,24 @@
-import { igdl } from "ruhend-scraper"
+import fetch from 'node-fetch'
 
-let handler = async (m, { args, conn }) => { 
-if (!args[0]) {
-return conn.reply(m.chat, '🍟 *Ingresa un link de Instagram*', m, rcanal)}
+let handler = async (m, { conn, usedPrefix, command, args }) => {
+if (!args[0]) return m.reply(`Ingresa un enlace de Instagram`)
+    
 try {
-await m.react(rwait)
-conn.reply(m.chat, `🕒 *Enviando El Video...*`, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: dev,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})      
-let res = await igdl(args[0])
-let data = res.data       
-for (let media of data) {
-await new Promise(resolve => setTimeout(resolve, 2000))           
-await conn.sendFile(m.chat, media.url, 'instagram.mp4', '🍟 *Tu video de instagram.*\n' + textbot, fkontak)
-}} catch {
-await m.react(error)
-conn.reply(m.chat, '🚩 Ocurrió un error.', m, fake)}}
+let api = await fetch(`https://deliriussapi-oficial.vercel.app/download/instagram?url=${args[0]}`)
+let json = await api.json()
+let { data } = json
+let JT = data
+for (let i = 0; i < JT.length; i++) {
+let HFC = JT[i];
+if (HFC.type === "image") {
+await conn.sendMessage(m.chat, { image: { url: HFC.url } }, { quoted: m })
+} else if (HFC.type === "video") {
+await conn.sendMessage(m.chat, { video: { url: HFC.url } }, { quoted: m })
+}}
+} catch (error) {
+console.error(error)
+}}
 
-handler.command = ['instagram', 'ig']
-handler.tags = ['descargas']
-handler.help = ['instagram', 'ig']
-handler.cookies = 1
-handler.register = true
+handler.command = /^(igdl|ig|instagramdl|instagram)$/i
 
 export default handler
