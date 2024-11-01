004f76511cb58ef0dd3316c084466002febe3552
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-11-01 17:13:17 -0500

    Update Convertidor•tourl.js

--- plugins/Convertidor•tourl.js
+++ plugins/Convertidor•tourl.js
@@ -5,7 +5,7 @@ import fetch from 'node-fetch'
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) return conn.reply(m.chat, '🍿 Responde a una Imagen', m, rcanal)
+  if (!mime) return conn.reply(m.chat, '🍿 Responde a una Imagen', m)
   await m.react(rwait)
   try {
   /*conn.reply(m.chat, '🚩 Convirtiendo la imagen en url...', m, {
@@ -25,7 +25,7 @@ let handler = async (m) => {
       txt += `› Expiración : ${isTele ? 'No expira' : 'Desconocido'}\n\n`
       txt += `> *🌵 Desarrollado por Daniel*`
 
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, fkontak, rcanal)
+await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m)
 await m.react(done)
 } catch {
 await conn.reply(m.chat, '🌱 Ocurrió un error', m, fake)
