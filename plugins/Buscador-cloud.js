43a558326d4c63896fd98f09c5bec06eb70c2c60
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-11-01 18:30:18 -0500

    Update Buscador-cloud.js

--- plugins/Buscador-cloud.js
+++ plugins/Buscador-cloud.js
@@ -6,9 +6,10 @@ if (!text) return conn.reply(m.chat, '*Ingresa el texto de lo que quieres buscar
 try {
 let api = await fetch(`https://apis-starlights-team.koyeb.app/starlight/soundcloud-search?text=${encodeURIComponent(text)}`);
 
-if (!results || results.length === 0) return conn.reply(m.chat, 'No se encontraron resultados', m);
+let json = await api.json();
+if (!Array.isArray(json) || json.length === 0) return conn.reply(m.chat, 'No se encontraron resultados.', m);
 
-  const firstTrack = results[0];
+  const firstTrack = json[0];
     const firstTrackInfo = `✨ *Primer Resultado* ✨\n\n` +
                            `*» Título* : ${firstTrack.title}\n` +
                            `*» Artista* : ${firstTrack.artists}\n` +
