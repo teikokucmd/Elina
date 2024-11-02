86dc3c6be1782e1d23967e1f7e8da811ec0bf922
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-11-02 14:50:23 -0500

    Update Descargas•modapk.js

--- plugins/Descargas•modapk.js
+++ plugins/Descargas•modapk.js
@@ -18,7 +18,7 @@ await conn.sendMessage(m.chat, { document: { url: URL }, mimetype: 'application/
 }
 handler.tags = ['descargas']
 handler.help = ['apkmod']
-handler.command = /^(apkmod|apk|dapk2|aptoide|aptoidedl)$/i
+handler.command = /^(aptoidedl)$/i
 handler.register = false
 handler.estrellas = 1
 
