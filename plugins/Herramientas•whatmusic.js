9b085765f1b7b618a29c587d11bc78733ca1a24f
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-11-02 17:03:21 -0500

    Update Herramientas•whatmusic.js

--- plugins/Herramientas•whatmusic.js
+++ plugins/Herramientas•whatmusic.js
@@ -1,34 +1,41 @@
-import fs from 'fs'
 import acrcloud from 'acrcloud'
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
+import yts from 'yt-search'
+
 let acr = new acrcloud({
-host: 'identify-eu-west-1.acrcloud.com',
-access_key: 'c33c767d683f78bd17d4bd4991955d81',
-access_secret: 'bvgaIAEtADBTbLwiPGYlxupWqkNGIjT7J9Ag2vIu'
+  host: 'identify-eu-west-1.acrcloud.com',
+  access_key: 'c33c767d683f78bd17d4bd4991955d81',
+  access_secret: 'bvgaIAEtADBTbLwiPGYlxupWqkNGIjT7J9Ag2vIu'
 })
-
-let handler = async (m) => {
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || ''
-if (/audio|video/.test(mime)) {
-let media = await q.download()
-let ext = mime.split('/')[1]
-fs.writeFileSync(`./tmp/${m.sender}.${ext}`, media)
-let res = await acr.identify(fs.readFileSync(`./tmp/${m.sender}.${ext}`))
-let { code, msg } = res.status
-if (code !== 0) throw msg
-let { title, artists, album, genres, release_date } = res.metadata.music[0]
-let txt = `
-𝙍𝙀𝙎𝙐𝙇𝙏𝘼𝘿𝙊 𝘿𝙀 𝙇𝘼 𝘽𝙐𝙎𝙌𝙐𝙀𝘿𝘼𝙎 
-
-• 🌻 𝙏𝙄𝙏𝙐𝙇𝙊: ${title}
-• 🍃 𝘼𝙍𝙏𝙄𝙎𝙏𝘼: ${artists !== undefined ? artists.map(v => v.name).join(', ') : 'No encontrado'}
-• 💻 𝘼𝙇𝘽𝙐𝙈: ${album.name || 'No encontrado'}
-• 🍂 𝙂𝙀𝙉𝙀𝙍𝙊: ${genres !== undefined ? genres.map(v => v.name).join(', ') : 'No encontrado'}
-• 🪙 𝙁𝙀𝘾𝙃𝘼 𝘿𝙀 𝙇𝘼𝙉𝙕𝘼𝙈𝙄𝙀𝙉𝙏𝙊: ${release_date || 'No encontrado'}
-`.trim()
-fs.unlinkSync(`./tmp/${m.sender}.${ext}`)
-m.reply(txt)
-} else throw '💭 Responda A Un Audio O Video'
+let handler = async (m, { conn, usedPrefix, command }) => {
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || q.mediaType || ''
+  if (/video|audio/.test(mime)) {
+  let buffer = await q.download()
+  let user = global.db.data.users[m.sender]
+  await m.react('🕓')
+  let { status, metadata } = await acr.identify(buffer)
+  if (status.code !== 0) throw status.msg 
+  let { title, artists, album, genres, release_date } = metadata.music[0]
+  let res = await yts(title)
+  let vid = res.videos[0]
+  let v = vid.url
+  let yt = await youtubedl(v).catch(async () => await youtubedlv2(v))
+  let url = await yt.audio['128kbps'].download()
+  let title2 = await yt.title
+  let txt = '`乂  W H A T M U S I C  -  T O O L S`\n\n'
+      txt += `	✩   *Titulo* : ${title}${artists ? `\n	✩   *Artists* : ${artists.map(v => v.name).join(', ')}` : ''}`
+      txt += `${album ? `\n	✩   *Album* : ${album.name}` : ''}${genres ? `\n	✩   *Genero* : ${genres.map(v => v.name).join(', ')}` : ''}\n`
+      txt += `	✩   *Fecha de lanzamiento* : ${release_date}\n\n`
+      txt += `> 🚩 *${textbot}*`
+  await conn.sendFile(m.chat, vid.thumbnail, 'thumbnail.jpg', txt, m)
+  await conn.sendFile(m.chat, url, title2 + '.mp3', null, m, false, { mimetype: 'audio/mpeg', asDocument: user.useDocument })
+  await m.react('✅')
+  } else return conn.reply(m.chat, `🚩 Etiqueta un audio o video de poca duración con el comando *${usedPrefix + command}* para ver que música contiene.`, m)
 }
-handler.command = ['quemusica', 'quemusicaes', 'whatmusic']
+handler.help = ['whatmusic *<audio/video>*']
+handler.tags = ['tools']
+handler.command = ['whatmusic', 'shazam']
+//handler.limit = 1
+handler.register = true 
 export default handler
