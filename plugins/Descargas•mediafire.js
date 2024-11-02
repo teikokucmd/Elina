e9c1c9d5eb3491c8cacb705c874c0b68dd7bb770
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-11-02 17:46:38 -0500

    Update Descargas•mediafire.js

--- plugins/Descargas•mediafire.js
+++ plugins/Descargas•mediafire.js
@@ -4,8 +4,8 @@ import cheerio from 'cheerio'
 import {mediafiredl} from '@bochilteam/scraper'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return conn.reply(m.chat, '🚩 Ingrese el enlace de un archivo de Mediafire.', m, rcanal)
-if (!args[0].match(/mediafire/gi)) return conn.reply(m.chat, '🍟 El enlace deve ser de un archivo de Mediafire.', m, rcanal)
+if (!args[0]) return conn.reply(m.chat, '🍭 Ingrese el enlace de un archivo de Mediafire.', m)
+if (!args[0].match(/mediafire/gi)) return conn.reply(m.chat, '🍟 El enlace deve ser de un archivo de Mediafire.', m)
 try {
 await m.react(rwait)
 let { title, ext, aploud, size, dl_url } = await mediafiredl(args[0])
@@ -16,7 +16,7 @@ let txt = `乂  *¡MEDIAFIRE - DESCARGAS!*  乂\n\n`
     txt += `✩ *MimeType* : ${ext}\n\n`
     txt += `*- ↻ El archivo se esta enviando espera un momento, soy lento. . .*`
 let img = await (await fetch('https://i.ibb.co/wLQFn7q/logo-mediafire.jpg')).buffer()
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, fkontak, null, rcanal)
+await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m)
 await conn.sendFile(m.chat, dl_url, title, null, fkontak, null, { mimetype: ext, asDocument: true })
 await m.react(done)
 } catch {
