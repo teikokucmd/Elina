1107e007ea21c9936f0146ea2a5866121e5bacc6
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-11-02 06:56:02 -0500

    Update and rename Ai•Bing.js to Ai•bingimg.js

--- plugins/Ai•bingimg.js
+++ plugins/Ai•bingimg.js
@@ -4,18 +4,18 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
   if (!text) return m.reply(`Ingresa una peticion`);
 
 try {
-let api = await fetch(`https://widipe.com/bingai?text=${text}`);
+let api = await fetch(`https://widipe.com/bingimg?text=${text}`);
 let json = await api.json()
-if (json.status) {
-m.reply(json.result);
+if (json.status && json.result.length > 0) {
+await conn.sendFile(m.chat, json.result[0], 'imagen.jpg', text, m);
 } else {
-m.reply('error :v');
+m.reply('error :v')
 }
 } catch (error) {
 console.error(error);
 }}
 
-handler.command = /^(bing)$/i
+handler.command = /^(bingimg)$/i
 
 export default handler
 
