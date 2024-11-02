9f026e99a8286e5f40bb999f5ba5171d3beeefef
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-11-02 10:24:51 -0500

    Update Descargas•modapk.js

--- plugins/Descargas•modapk.js
+++ plugins/Descargas•modapk.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
-if (!args[0]) throw `\`\`\`[ 🌟 ] Ingresa el nombre de la aplicación que quieres descargar. Ejemplo:\n${usedPrefix + command} SonGoku\`\`\``
+if (!args[0]) throw `🍭 ingrese el nombre de la app que desea buscar`
 let res = await fetch(`https://api.dorratz.com/v2/apk-dl?text=${args[0]}`);
 let result = await res.json();
 let { name, size, lastUpdate, icon } = result;
