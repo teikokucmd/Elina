16367e4f80adbd0802d4296b9229174fb763333d
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-11-01 20:13:29 -0500

    Update Sticker•sticker.js

--- plugins/Sticker•sticker.js
+++ plugins/Sticker•sticker.js
@@ -40,7 +40,7 @@ else return m.reply(`💫 El url es incorrecto`)
 console.error(e)
 if (!stiker) stiker = e
 } finally {
-if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: ``, body: ``, mediaType: 2, sourceUrl: ``, thumbnail: ``}}}, { quoted: m })
+if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m)
 
 else return conn.reply(m.chat, '🍿 responde a un video o imagen', m)
 
