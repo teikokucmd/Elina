fe2c5f573095427d9d56b2291302d56bab38ede4
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-11-02 08:39:02 -0500

    Update Herramientas•ssweb.js

--- plugins/Herramientas•ssweb.js
+++ plugins/Herramientas•ssweb.js
@@ -1,19 +1,19 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command, args }) => {
-if (!args[0]) return conn.reply(m.chat, '⚠️ *Ingrese el Link de una página.*', m, rcanal)
+if (!args[0]) return conn.reply(m.chat, 'Ingrese el Link de una página', m)
 try {
 await m.react(rwait)
-conn.reply(m.chat, '🚀 Buscando su información....', m, {
+/*conn.reply(m.chat, '🚀 Buscando su información....', m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: dev,
 previewType: 0, thumbnail: icons, 
-sourceUrl: channel }}})
+sourceUrl: channel }}})*/
 let ss = await (await fetch(`https://image.thum.io/get/fullpage/${args[0]}`)).buffer()
-conn.sendFile(m.chat, ss, 'error.png', args[0], fkontak)
+conn.sendFile(m.chat, ss, 'error.png', args[0], m)
 await m.react(done)
 } catch {
-return conn.reply(m.chat, '🚩 Ocurrió un error.', m, fake)
+return conn.reply(m.chat, '🚩 Ocurrió un error.', m)
 await m.react(error)}}
 handler.help = ['ssweb', 'ss']
 handler.tags = ['tools']
