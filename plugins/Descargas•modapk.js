a78ca52a693cd65145bd88fd4c8f088184cff084
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-11-02 10:19:52 -0500

    Update Descargas•modapk.js

--- plugins/Descargas•modapk.js
+++ plugins/Descargas•modapk.js
@@ -1,33 +1,25 @@
-import fetch from 'node-fetch';
-
-let handler = async (m, { conn, usedPrefix, command, text }) => {
-if (!text) return m.reply(`Ingresa el nombre de la aplicación que deseas buscar`)
+let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
+if (!args[0]) throw `\`\`\`[ 🌟 ] Ingresa el nombre de la aplicación que quieres descargar. Ejemplo:\n${usedPrefix + command} SonGoku\`\`\``
+let res = await fetch(`https://api.dorratz.com/v2/apk-dl?text=${args[0]}`);
+let result = await res.json();
+let { name, size, lastUpdate, icon } = result;
+let URL = result.dllink
+let packe = result.package
+let texto = `  ❯───「 𝗔𝗣𝗞 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗 」───❮
+     Nombre : ⇢ ${name} 📩
+     Tamaño : ⇢ ${size} ⚖️
+     Package : ⇢ ${packe} 📦
+    Actulizado : ⇢ ${lastUpdate} 🗓️
     
-try {
-let api = await fetch(`https://deliriussapi-oficial.vercel.app/download/apk?query=${text}`)
-let json = await api.json()
-let { name, publish, id, size, image, download, developer, store, stats } = json.data;
+## Su aplicación se enviará en un momento POR FAVOR ESPERE . . .`
+await conn.sendFile(m.chat, icon, name + '.jpg', texto, m)
 
-let JT = `*Nombre:* ${name}
-*Publicado:* ${publish}
-*ID de App:* ${id}
-*Tamaño:* ${size}
-*Desarrollador:* ${developer}
-*Descargas:* ${stats.downloads}
-*Rating:* ${stats.rating.average} (de ${stats.rating.total} votos)`;
-
-await conn.sendFile(m.chat, image, `HasumiBotFreeCodes.png`, appInfo, m);
-await conn.sendMessage(m.chat, { document: { url: download }, mimetype: 'application/vnd.android.package-archive', fileName: name + '.apk', caption: null }, { quoted: m })
-
-if (store.avatar) {
-await conn.sendFile(m.chat, store.avatar, `HasumiBotFreeCodes.jpg`, `*Tienda:* ${store.name}`, m);
+await conn.sendMessage(m.chat, { document: { url: URL }, mimetype: 'application/vnd.android.package-archive', fileName: name + '.apk', caption: ''}, { quoted: m });
 }
-
-} catch (error) {
-console.error(error);
-}}
-
-handler.command = /^(apk)$/i
+handler.tags = ['descargas']
+handler.help = ['apkmod']
+handler.command = /^(apkmod|apk|dapk2|aptoide|aptoidedl)$/i
+handler.register = false
+handler.estrellas = 1
 
 export default handler
-
