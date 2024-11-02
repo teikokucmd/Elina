4e2e5e10792b8a3a33428fa4c8f1311b1f7a3926
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-11-02 06:51:12 -0500

    Update Ai•gemini.js

--- plugins/Ai•gemini.js
+++ plugins/Ai•gemini.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 var handler = async (m, { text,  usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `『🪐』𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙪𝙣 𝙩𝙚𝙭𝙩𝙤 𝙥𝙖𝙧𝙖 𝙪𝙨𝙖𝙧 𝙚𝙨𝙩𝙚 𝙘𝙤𝙢𝙖𝙣𝙙𝙤.\n\n• 𝙋𝙤𝙧 𝙚𝙟𝙚𝙢𝙥𝙡𝙤:\n${usedPrefix + command} Hola`, m, rcanal)
+if (!text) return conn.reply(m.chat, `Ingrese una petición para interactuar con gemini`, m)
 try {
 await m.react(rwait)
 conn.sendPresenceUpdate('composing', m.chat)
@@ -9,7 +9,7 @@ var res = await apii.json()
 await m.reply(res.result)
 } catch {
 await m.react(error)
-await conn.reply(m.chat, `『📣』𝙊𝙘𝙪𝙧𝙧𝙞𝙤 𝙪𝙣 𝙚𝙧𝙧𝙤𝙧 𝙚𝙣 𝙚𝙡 𝙘𝙤𝙢𝙖𝙣𝙙𝙤, 𝙧𝙚𝙥𝙤𝙧𝙩𝙖𝙡𝙤 𝙖𝙡 𝙘𝙧𝙚𝙖𝙙𝙤𝙧 𝙙𝙚𝙡 𝙗𝙤𝙩.`, m, rcanal)
+await conn.reply(m.chat, `Error.`, m)
 }}
 handler.command = ['gemini']
 handler.help = ['gemini']
