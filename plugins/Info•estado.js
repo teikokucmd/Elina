01a06a214c720d80bd5487c54dee4a7b218ba803
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-11-02 06:34:58 -0500

    Update Info•estado.js

--- plugins/Info•estado.js
+++ plugins/Info•estado.js
@@ -42,7 +42,7 @@ var txt = `🍟 Estado De La Bot
 *Internet IN* : ${netsIn}
 *Internet OUT* : ${netsOut}`
 
-conn.relayMessage(m.chat, { extendedTextMessage:{ text: txt, contextInfo: { externalAdReply: { title: '', mediaType: 1, previewType: 0, renderLargerThumbnail: true, thumbnailUrl: 'https://telegra.ph/file/ec8cf04e3a2890d3dce9c.jpg', sourceUrl: '' }}, mentions: [m.sender] }}, {})
+conn.relayMessage(m.chat, txt, m)
 
 console.log(OS)
 } catch (e) {
