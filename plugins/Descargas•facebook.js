5f0b9858866d995b7d5dffb2b514758fd598b3fb
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-11-02 17:36:55 -0500

    Update Descargas•facebook.js

--- plugins/Descargas•facebook.js
+++ plugins/Descargas•facebook.js
@@ -1,46 +1,29 @@
-import { igdl } from 'ruhend-scraper'
+import { facebookdl, facebookdlv2 } from '@bochilteam/scraper';
 
-const handler = async (m, { text, conn, args, usedPrefix, command }) => {
-if (!args[0]) {
-return conn.reply(m.chat, '🍟 *Ingresa Un Link De Facebook*', m, rcanal)}
-let res
-try {
-conn.reply(m.chat, `🕒 *Descargando su video de facebook.*`, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: dev,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
-await m.react(rwait)
-res = await igdl(args[0])
-} catch {
-await m.react(error)
-return conn.reply(m.chat, '🚩 *Error al obtener datos. Verifica el enlace.*', m, fake)}
-let result = res.data
-if (!result || result.length === 0) {
-return conn.reply(m.chat, '🚩 *No se encontraron resultados.*', m, fake)}
-let data
-try {
-await m.react(rwait)
-data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "360p (SD)")
-} catch {
-await m.react(error)
-return conn.reply(m.chat, '🚩 *Error al procesar los datos.*', m, rcanal)}
-if (!data) {
-return conn.reply(m.chat, '🚩 *No se encontró una resolución adecuada.*', m, rcanal)}
-let video = data.url
-try {
-await m.react(rwait)
-await conn.sendMessage(m.chat, { video: { url: video }, caption: '🍟 *Tu video de facebook.*\n' + textbot, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: fkontak })
-await m.react(done)
-} catch {
-await m.react(error)
-return conn.reply(m.chat, '🚩 *Error al enviar el video.*', m, rcanal)}}
+const handler = async (m, { conn, args }) => {
+if (!args[0]) return conn.reply(m.chat, 'Ingresa un enlace de facebook', m);
 
-handler.help = ['facebook', 'fb']
-handler.tags = ['descargas']
-handler.command = ['facebook', 'fb']
-handler.cookies = 1
-handler.register = true
+try {
+const { result } = await facebookdl(args[0]).catch(() => facebookdlv2(args[0]))
 
-export default handler
+if (result && result.length > 0) {
+let videoSent = false;
+
+for (const item of result) {
+if (item.isVideo) {
+const fileType = item.ext || 'mp4';
+await conn.sendFile(m.chat, item.url, `facebook.${fileType}`, null, m);
+videoSent = true;
+break;
+}
+}
+} else {
+conn.reply(m.chat, 'Sin resultados', m);
+}
+} catch {
+conn.reply(m.chat, 'ocurrio un error :v', m);
+}
+};
+
+handler.command = ['facebook'];
+export default handler;
