f829fd4368187fdb8c3fdc4d2e8a1f2b85150f8b
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-11-02 15:46:13 -0500

    Update Descargas•tiktokvid.js

--- plugins/Descargas•tiktokvid.js
+++ plugins/Descargas•tiktokvid.js
@@ -6,17 +6,17 @@ let handler = async (m, { conn, usedPrefix, command, text, args }) => {
   try {
     let { title, author, duration, views, likes, comments_count, share_count, download_count, published, dl_url } = await Starlights.tiktokvid(text)
 
-      let txt = '`乂  T I K T O K  -  D O W N L O A D`\n\n'
-          txt += `    ✩  *Título* : ${title}\n`
-          txt += `    ✩  *Autor* : ${author}\n`
-          txt += `    ✩  *Duración* : ${duration} segundos\n`
-          txt += `    ✩  *Vistas* : ${views}\n`
-          txt += `    ✩  *Likes* : ${likes}\n`
-          txt += `    ✩  *Comentarios* : ${comments_count}\n`
-          txt += `    ✩  *Compartidos* : ${share_count}\n`
-          txt += `    ✩  *Publicado* : ${published}\n`
-          txt += `    ✩  *Descargas* : ${download_count}\n\n`
-          txt += `> 🚩 ${textbot}`
+      let txt = ' *ゲ◜៹ Tik tok - Video ៹◞ゲ*\n\n'
+          txt += `› *Título* : ${title}\n`
+          txt += `› *Autor* : ${author}\n`
+          txt += `› *Duración* : ${duration} segundos\n`
+          txt += `› *Vistas* : ${views}\n`
+          txt += `› *Likes* : ${likes}\n`
+          txt += `› *Comentarios* : ${comments_count}\n`
+          txt += `› *Compartidos* : ${share_count}\n`
+          txt += `› *Publicado* : ${published}\n`
+          txt += `› *Descargas* : ${download_count}\n\n`
+          txt += `🍭 *Powered By Daniel (神志不清)*`
 
       await conn.sendFile(m.chat, dl_url, `thumbnail.mp4`, txt, m)
       await m.react('✅')
