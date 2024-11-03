6c5a027ae2143a289e333376c297458182d3fae5
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-11-03 09:35:38 -0500

    Update Owner•dsowner.js

--- plugins/Owner•dsowner.js
+++ plugins/Owner•dsowner.js
@@ -6,9 +6,9 @@ import path from 'path'
 var handler = async (m, { conn, usedPrefix }) => {
 
 if (global.conn.user.jid !== conn.user.jid) {
-return conn.reply(m.chat, '🚩 *Utiliza este comando directamente en el número principal del Bot*', m, rcanal, )
+return conn.reply(m.chat, '🍭 *Utiliza este comando directamente en el número principal del Bot*', m)
 }
-await conn.reply(m.chat, '🚩 *Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json...*', m, rcanal, )
+await conn.reply(m.chat, '🍭 *Iniciando proceso de eliminación de todos los archivos de sesión, excepto el archivo creds.json...*', m)
 m.react(rwait)
 
 let sessionPath = `./${sessions}/`
@@ -16,7 +16,7 @@ let sessionPath = `./${sessions}/`
 try {
 
 if (!existsSync(sessionPath)) {
-return await conn.reply(m.chat, '🚩 *La carpeta está vacía*', m, rcanal, )
+return await conn.reply(m.chat, '🍭 *La carpeta está vacía*', m)
 }
 let files = await fs.readdir(sessionPath)
 let filesDeleted = 0
@@ -27,16 +27,16 @@ filesDeleted++;
 }
 }
 if (filesDeleted === 0) {
-await conn.reply(m.chat, '🚩 *La carpeta esta vacía*',  m, rcanal, )
+await conn.reply(m.chat, '🍭 *La carpeta esta vacía*',  m)
 } else {
 m.react(done)
-await conn.reply(m.chat, `🚩 *Se eliminaron ${filesDeleted} archivos de sesión, excepto el archivo creds.json*`,  m, rcanal, )
-conn.reply(m.chat, `🚩 *¡Hola! ¿logras verme?*`, m, rcanal, )
+await conn.reply(m.chat, `🍭 *Se eliminaron ${filesDeleted} archivos de sesión, excepto el archivo creds.json*`,  m)
+conn.reply(m.chat, `🍭 *¡Hola! ¿logras verme?*`, m)
 
 }
 } catch (err) {
 console.error('Error al leer la carpeta o los archivos de sesión:', err);
-await conn.reply(m.chat, '🚩 *Ocurrió un fallo*',  m, rcanal, )
+await conn.reply(m.chat, '🍭 *Ocurrió un fallo*',  m)
 }
 
 }
