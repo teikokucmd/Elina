12bb8839948e2d910d32b6de25b2f85a2dce322e
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-11-02 20:55:09 -0500

    Update Buscador•google.js

--- plugins/Buscador•google.js
+++ plugins/Buscador•google.js
@@ -1,27 +1,35 @@
-import {googleIt} from '@bochilteam/scraper'
-import google from 'google-it'
-import axios from 'axios'
-let handler = async (m, { conn, command, args, usedPrefix }) => {
-const fetch = (await import('node-fetch')).default;
-const text = args.join` `
-if (!text) return conn.reply(m.chat, '🍭 Ingresa lo que deseas buscar en Google.', m)
-/*conn.reply(m.chat, `🚩 Buscando Su Información...`, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: dev,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})*/
-const url = 'https://google.com/search?q=' + encodeURIComponent(text)
-google({'query': text}).then(res => {
-let teks = `Resultado de : ${text}\n\n`
-for (let g of res) {
-teks += `Titulo: ${g.title}\nInfo: ${g.snippet}\nUrl: ${g.link}\n\n`
+import axios from 'axios';
+
+let handler = async (m, { conn, command, args }) => {
+  const text = args.join(' ');
+  if (!text) return conn.reply(m.chat, '🍭 Ingresa lo que deseas buscar junto al comando.', m);
+  
+  await m.react('🕓');
+  let img = 'https://i.ibb.co/P5kZNFF/file.jpg';
+  const url = `https://widipe.com/googlesearch?query=${encodeURIComponent(text)}`;
+
+  try {
+    const response = await axios.get(url, { headers: { accept: 'application/json' } });
+    const results = response.data.result;
+
+    if (results.length > 0) {
+      let teks = `*ゲ◜៹ Google Search ៹◞ゲ*\n\n`;
+      for (let g of results) {
+        teks += `*${g.title}*\n${g.link}\n${g.description}\n\n`;
+      }
+      conn.sendFile(m.chat, img, 'thumbnail.jpg', teks, m).then(() => m.react('✅'));
+    } else {
+      conn.reply(m.chat, '❌ No se encontraron resultados.', m);
+    }
+  } catch (error) {
+    console.error(error);
+    conn.reply(m.chat, '❌ Error al realizar la búsqueda.', m);
+  }
 }
-conn.reply(m.chat, teks, m)
-})
-}
-handler.help = ['google <búsqueda>']
-handler.tags = ['buscador']
-handler.command = ['google']
-handler.register = true 
-export default handler
+
+handler.help = ['googlesearch *<texto>*'];
+handler.tags = ['tools', 'search'];
+handler.command = /^googlesearch|google$/i;
+handler.register = true;
+
+export default handler;
