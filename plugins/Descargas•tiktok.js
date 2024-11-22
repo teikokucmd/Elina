8edd35f845d3c94ba92a46ea920ee20cc99fff12
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-11-22 09:28:19 -0500

    Update Descargas•tiktok.js

--- plugins/Descargas•tiktok.js
+++ plugins/Descargas•tiktok.js
@@ -1,14 +1,15 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { args, conn }) => {
-if (!args[0]) return m.reply("ingresa un url de tiktok")
-
+if (!args[0]) return m.reply("ingresa un enlace de tiktok")
+    
 try {
-let api = await fetch(`https://exonity.tech/api/tiktok?url=${args[0]}`)
+let api = await fetch(`https://tools.betabotz.eu.org/tools/tiktokdl?url=${args[0]}`)
 let json = await api.json()
-let { server1, serverHD, audio } = json.result
-await conn.sendFile(m.chat, serverHD.url, 'HasumiBotFreeCodes.mp4', 'Calidad HD', m) // Cambiar a server1.url para que sea calidad mediana
-await conn.sendFile(m.chat, audio, 'HasumiBotFreeCodes.mp3', null, m)
+let { processed_time:proceso } = json.result    
+let { cover:imgvid, title, ai_dynamic_cover:svid, origin_cover:img, duration:duracion, play:video, wmplay:videomarcadeagua, music, } =  json.result.data
+await conn.sendFile(m.chat , video, 'HasumiBotFreeCodes.mp4', title, m)
+await conn.sendFile(m.chat , music, 'HasumiBotFreeCodes.mp3', null, m)
 } catch (error) {
 console.error(error)
 }}
