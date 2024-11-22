77e6c73ae3e7a36be23a844c75fa007e87f530a3
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-11-22 09:26:52 -0500

    Update Descargas•tiktok.js

--- plugins/Descargas•tiktok.js
+++ plugins/Descargas•tiktok.js
@@ -4,11 +4,11 @@ let handler = async (m, { args, conn }) => {
 if (!args[0]) return m.reply("ingresa un url de tiktok")
 
 try {
-let api = await fetch(`https://exonity.tech/api/tiktok2?url=${args[0]}`)
+let api = await fetch(`https://exonity.tech/api/tiktok?url=${args[0]}`)
 let json = await api.json()
-let { cover:img, title, origin_cover, no_watermark, watermark, music, like, author } = json.result
-await conn.sendFile(m.chat, no_watermark, 'HasumiBotFreeCodes.mp4', title, m) 
-await conn.sendFile(m.chat, music, 'HasumiBotFreeCodes.mp3', null, m)
+let { server1, serverHD, audio } = json.result
+await conn.sendFile(m.chat, serverHD.url, 'HasumiBotFreeCodes.mp4', 'Calidad HD', m) // Cambiar a server1.url para que sea calidad mediana
+await conn.sendFile(m.chat, audio, 'HasumiBotFreeCodes.mp3', null, m)
 } catch (error) {
 console.error(error)
 }}
