2fc8018111973cc420a264967e7e2dbba958c180
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-11-30 18:18:58 -0500

    Update Descargas•tiktok.js

--- plugins/Descargas•tiktok.js
+++ plugins/Descargas•tiktok.js
@@ -19,26 +19,26 @@ const handler = async (m, {conn, text, args, usedPrefix, command}) => {
   } catch (ee1) {
   try {
     const dataF = await tiktok.v1(args[0]);
-    const desc1 = `_💌  ᩭ✎Tiktok sin marca de agua descargado con éxito_`;
-    await conn.sendMessage(m.chat, {video: {url: dataF.play}, caption: desc1}, {quoted: fkontak});
+    const desc1 = `Descargado con éxito`;
+    await conn.sendMessage(m.chat, {video: {url: dataF.play}, caption: desc1}, {quoted: m});
   } catch (e1) {
     try {
       const tTiktok = await tiktokdlF(args[0]);
-      const desc2 = `_💌  ᩭ✎Tiktok sin marca de agua descargado con éxito_`;
-      await conn.sendMessage(m.chat, {video: {url: tTiktok.video}, caption: desc2}, {quoted: fkontak});
+      const desc2 = `Descargado con exito`;
+      await conn.sendMessage(m.chat, {video: {url: tTiktok.video}, caption: desc2}, {quoted: m});
     } catch (e2) {
       try {
         const p = await fg.tiktok(args[0]);
-        const te = `_💌  ᩭ✎Tiktok sin marca de agua descargado con éxito_`;
-        await conn.sendMessage(m.chat, {video: {url: p.nowm}, caption: te}, {quoted: fkontak});
+        const te = `Descargado con exito`;
+        await conn.sendMessage(m.chat, {video: {url: p.nowm}, caption: te}, {quoted: m});
       } catch (e3) {
         try {
           const {author: {nickname}, video, description} = await tiktokdl(args[0]);
           const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd;
-          const cap = `_💌  ᩭ✎Tiktok sin marca de agua descargado con éxito_`;
-          await conn.sendMessage(m.chat, {video: {url: url}, caption: cap}, {quoted: fkontak});
+          const cap = `Descargado con exito`;
+          await conn.sendMessage(m.chat, {video: {url: url}, caption: cap}, {quoted: m});
         } catch {
-        return conn.reply(m.chat, `🚩 Ocurrió un error.`, m);
+        return conn.reply(m.chat, `🍭 Ocurrió un error.`, m);
           }
         }
       }
@@ -52,7 +52,7 @@ handler.register = true;
 export default handler;
 
 async function tiktokdlF(url) {
-  if (!/tiktok/.test(url)) return conn.reply(m.chat, `🚩 Te faltó el link del video de tiktok.`, m);
+  if (!/tiktok/.test(url)) return conn.reply(m.chat, `🍭 Te faltó el link del video de tiktok.`, m);
   const gettoken = await axios.get('https://tikdown.org/id');
   const $ = cheerio.load(gettoken.data);
   const token = $('#download-form > input[type=hidden]:nth-child(2)').attr( 'value' );
