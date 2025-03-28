cd47fe38a18dd53c614ab1cee0c70cc5b6411417
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2025-03-27 23:42:51 -0500

    Update Group•setprueba.js

--- plugins/Group•setprueba.js
+++ plugins/Group•setprueba.js
@@ -1,11 +1,12 @@
 const handler = async (m, {conn, text, isROwner, isOwner}) => {
 if (text) {
 global.db.data.chats[m.chat].sWelcome = text;
-m.reply('*✅ hola*');
-} else throw `*⚠️ INGRESE EL TEXTO*`;
+m.reply('*✅ HOLA*');
+} else throw `*Ingresé el texto*`;
 };
 handler.help = ['setwelcome <text>'];
 handler.tags = ['group'];
-handler.command = ['setprueba'];
+handler.command = ['setwelcome'];
 handler.admin = true;
-handler.register = true
+handler.register = true 
+export default handler;
