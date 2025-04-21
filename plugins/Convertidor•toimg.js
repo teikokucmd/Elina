c0d44fece2b26450d83fa253fe352855b9c71206
teikokucmd <comunidadteikoku@gmail.com>
2025-04-21 08:31:10 -0500

    Update Convertidor•toimg.js

--- plugins/Convertidor•toimg.js
+++ plugins/Convertidor•toimg.js
@@ -2,7 +2,7 @@ import { webp2png } from '../lib/webp2mp4.js'
 
 var handler = async (m, { conn, usedPrefix, command }) => {
 
-const notStickerMessage = `*🌳 Responda a una imagen*`
+const notStickerMessage = `*🖐🏻 Responda a una imagen*`
 if (!m.quoted) throw notStickerMessage 
 const q = m.quoted || m
 let mime = q.mediaType || ''
