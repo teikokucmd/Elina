39c7709c3dee85ac5a51016579da1ae2017feb0b
teikokucmd <comunidadteikoku@gmail.com>
2025-04-20 23:35:33 -0500

    Update Grupo•invitar.js

--- plugins/Grupo•invitar.js
+++ plugins/Grupo•invitar.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-if (!text) await m.reply(`🍟 Ingrese el número al que quiere enviar una invitación al grupo\n\n🚩 Ejemplo :\n*${usedPrefix + command}* 573218138672`)
-if (text.includes('+')) await m.reply('🚩 Ingrese el número todo sin el *+*')
-if (isNaN(text)) await m.reply('🍟 Ingrese sólo números más su código de país sin espacios')
+if (!text) await m.reply(`🌺Ingrese el número al que quiere enviar una invitación al grupo\n\n🚩 Ejemplo :\n*${usedPrefix + command}* 573218138672`)
+if (text.includes('+')) await m.reply('❌ Ingrese el número todo sin el *+*')
+if (isNaN(text)) await m.reply('🌺Ingrese sólo números más su código de país sin espacios')
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-await conn.reply(text+'@s.whatsapp.net', `🍟 *INVITACIÓN A GRUPO*\n\nUn usuario te invitó a unirte a este grupo \n\n${link}`, m, {mentions: [m.sender]})
-await m.reply(`🍟 Se envió un enlace de invitación al usuario.`) 
+await conn.reply(text+'@s.whatsapp.net', `🌺 *INVITACIÓN A GRUPO*\n\nUn usuario te invitó a unirte a este grupo \n\n${link}`, m, {mentions: [m.sender]})
+await m.reply(`🌺 Se envió un enlace de invitación al usuario.`) 
 
 }
 handler.help = ['invite *<numero>*']
